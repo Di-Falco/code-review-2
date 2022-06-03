37af741609a97a3aafbed910ed6918ed5c832f82
Anthony DiFalco <aodifalco@gmail.com>
2022-06-03 12:20:36 -0700

    add Hello World examples for each language

--- js/scripts.js
+++ js/scripts.js
@@ -9,25 +9,26 @@ $(document).ready(function() {
     const question2 = parseInt($("input:radio[name=question2]:checked").val());
     const question3 = parseInt($("input:radio[name=question3]:checked").val());
     const question4 = parseInt($("input:radio[name=question4]:checked").val());
-  //  const favoriteColor = $("#color").val();
+    const favoriteColor = $("#color").val();
 
     const recommendation = languageRec(question1, question2, question3, question4);
   
-  //  $("body").css("color", favoriteColor);
+    $("#colorPrompt").hide();
+    $("body").css("background-color", favoriteColor);
 
     $("#scratch,#python,#csharp,#brainf--k,#cplusplus,#javascript,#java").hide();
 
     if(recommendation === -11){
       $("#scratch").show();
-    }else if(recommendation <= -8){
+    }else if(recommendation <= -6){
       $("#python").show();
-    }else if(recommendation <= -5){
+    }else if(recommendation <= -2){
       $("#csharp").show();
     }else if(recommendation === 11){
       $("#brainf--k").show();
-    }else if(recommendation >= 8){
+    }else if(recommendation >= 6){
       $("#cplusplus").show();
-    }else if(recommendation >= 5){
+    }else if(recommendation >= 2){
       $("#javascript").show();
     }else{
       $("#java").show();
