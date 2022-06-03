9d0acc9b5915ba282aab4680268ecfeb80f00f2b
Anthony DiFalco <aodifalco@gmail.com>
2022-06-03 14:44:35 -0700

    WIP: add alert when user provides no input for one or more answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,9 @@
 function languageRec(q1, q2, q3, q4){
   return q1 + q2 + q3 + q4;
 }
-  
-$(document).ready(function() {
-  $("form#language-recommendation").submit(function(event) {
-    event.preventDefault();
-    const question1 = parseInt($("#experience").val());
-    const question2 = parseInt($("input:radio[name=question2]:checked").val());
-    const question3 = parseInt($("input:radio[name=question3]:checked").val());
-    const question4 = parseInt($("input:radio[name=question4]:checked").val());
-    const favoriteColor = $("#color").val();
 
-    const recommendation = languageRec(question1, question2, question3, question4);
-  
-    $("#colorPrompt").hide();
-    $("body").css("background-color", favoriteColor);
-
-    $("#scratch,#python,#csharp,#brainf--k,#cplusplus,#javascript,#java").hide();
-
+function result(recommendation) {
+  if( $("radio#q2").is(":checked") ){
     if(recommendation === -11){
       $("#scratch").show();
     }else if(recommendation <= -6){
@@ -33,5 +19,27 @@ $(document).ready(function() {
     }else{
       $("#java").show();
     }
+  }else{
+    alert("Please select an answer for each question");
+  }
+}
+  
+$(document).ready(function() {
+  $("form#language-recommendation").submit(function(event) {
+    event.preventDefault();
+    const question1 = parseInt($("#experience").val());
+    const question2 = parseInt($("input:radio[name=question2]:checked").val());
+    const question3 = parseInt($("input:radio[name=question3]:checked").val());
+    const question4 = parseInt($("input:radio[name=question4]:checked").val());
+    const favoriteColor = $("#color").val();
+
+    const recommendation = languageRec(question1, question2, question3, question4);
+  
+    $("body").css("background-color", favoriteColor);
+
+    $("#scratch,#python,#csharp,#brainf--k,#cplusplus,#javascript,#java").hide();
+
+    $(document).on("click", "#button", result(recommendation));
+
   });
 });
